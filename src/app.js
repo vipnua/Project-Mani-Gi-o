d48340894cee14cb1732e2b2533dacfa875b9051
vipnua <giangvipbg2002@gmail.com>
2022-08-01 00:18:28 +0700

    conflict

--- src/app.js
+++ src/app.js
@@ -12,21 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 app.use("/api", AuthRouter);
 app.use("/api", postRouter);
-
-// connect db
-
-// const conn = mongoose.createConnection("mongodb://localhost:27017/trangtin");
-// conn.on("connected", function () {
-//     console.log(`kết nối thành công với mongoose ::: ${this.name}`);
-// })
-
-// app.listen(process.env.PORT, () => {
-//     console.log("Kết nối thành công, cổng " + process.env.PORT);
-// });
 
 mongoose.connect("mongodb://localhost:27017/trangtin", () => {
     console.log("successfully");
