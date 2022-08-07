b7560cf793bdc038a5c1a518307840b5e8f29e5c
quanbon <quanbon1997@gmail.com>
2022-08-07 10:51:17 +0700

    #23 #24 #25 #26

---
+++ src/models/auth.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import user from '../models/auth.interface';
+// import { BinaryLike, createHmac } from "crypto";
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 225
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    role: {
+        type: Number,
+        default: 0,
+    },
+
+},{ timestamps: true });
+
+// userSchema.methods = {
+//     authenticate(password: any){
+//         return this.password === this.encrytPassword(password);
+//     },
+//     encrytPassword: (password: BinaryLike) => {
+//         if(!password) return;
+//         try {
+//             return createHmac('sha256','abc').update(password).digest("hex");
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     }
+
+// };
+
+// userSchema.pre("save", function (next) {
+//     this.password = this.encrytPassword(this.password);
+//     next();
+// });
+
+export default mongoose.model<user>("User", userSchema);
